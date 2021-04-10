138217d94f37a3b7740cc716ecd2ab33694a5a09
Rafael Marques <marquesrf.dev@outlook.com>
2021-04-10 00:23:33 -0300

    Added attributes and methods to: deposit and withdraw values, get the account balance and see account transactions

--- src/learning/projects/lp1/Account.java
+++ src/learning/projects/lp1/Account.java
@@ -1,19 +1,80 @@
 package learning.projects.lp1;
 
+import java.util.ArrayList;
+
 public class Account {
 	// Class variables
-	
+	private double balance;
+	private ArrayList<Double> transactions = new ArrayList<Double>();
+	private int clientID;
+	private String clientName;
+		
 	// Class constructor
+	Account(String cname, int cid) {
+		setClientID(cid);
+		setClientName(cname);
+	}
 	
-	// Method: money deposit
+	// Method: money deposit. TODO: add operation display text here?
+	void deposit(double value) {
+		if(value != 0.0) {
+			setBalance(value);
+			setTransaction(value);
+		}
+	}
 	
-	// Method: money withdraw
+	// Method: money withdraw. TODO: add operation display text here?
+	void withdraw(double value) {
+		if(value != 0.0) {
+			setBalance(-value);
+			setTransaction(-value);
+		}
+	}
 	
-	// Method: show balance
+	// Method: show balance. TODO: add display text here(or in the get method)
 	
 	// Method: estimate investment results
 	
 	// Method: display options menu
 	
-	// Method: show transactions history
+	// Method: show transactions history. TODO: add display text here(or in the get method)
+	void transactionsHistory() {
+		getTransactions();
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double value) {
+		this.balance += value;
+	}
+
+	public void getTransactions() {
+		if(!transactions.isEmpty()) {
+			transactions.forEach(System.out::println);			
+		} else {
+			System.out.println("No transactions found!");
+		}
+	}
+
+	public void setTransaction(double value) {
+		this.transactions.add(value);
+	}
+
+	public int getClientID() {
+		return clientID;
+	}
+
+	public void setClientID(int clientID) {
+		this.clientID = clientID;
+	}
+
+	public String getClientName() {
+		return clientName;
+	}
+
+	public void setClientName(String clientName) {
+		this.clientName = clientName;
+	}
 }
--- src/learning/projects/lp1/BankApp.java
+++ src/learning/projects/lp1/BankApp.java
@@ -3,7 +3,16 @@ package learning.projects.lp1;
 public class BankApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Account ac1 = new Account("Rafael", 1);
+		
+		ac1.deposit(5500.27);
+		ac1.withdraw(300);
+		System.out.println("Current Balance: " + ac1.getBalance()); // print
+		ac1.withdraw(150);
+		ac1.deposit(1230.00);
+		System.out.println("Current Balance: " + ac1.getBalance()); // print
+		System.out.println("Transaction history:");
+		ac1.getTransactions(); // print
 
 	}
 
