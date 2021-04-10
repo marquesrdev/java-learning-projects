d9bfd63314e48da182dca2c3d273a19a42aceec7
Rafael Marques <marquesrf.dev@outlook.com>
2021-04-09 23:04:02 -0300

    Added 'Account' class

---
+++ src/learning/projects/lp1/Account.java
@@ -0,0 +1,19 @@
+package learning.projects.lp1;
+
+public class Account {
+	// Class variables
+	
+	// Class constructor
+	
+	// Method: money deposit
+	
+	// Method: money withdraw
+	
+	// Method: show balance
+	
+	// Method: estimate investment results
+	
+	// Method: display options menu
+	
+	// Method: show transactions history
+}
